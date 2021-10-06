9aa1c869e6a11c1765ac6c49cf43abbdebbc01d3
w015319k <w015319k@C108-28>
2021-10-06 16:22:45 +0100

    Currently at line 45

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Hangman
 {
@@ -18,24 +19,39 @@ namespace Hangman
         int m_Lives = 6;
         List<char> m_IncorrectLetters;
         List<char> m_CorrectLetters;
-        string[] m_Words = { };
+        string[] m_Words = {"Bath" , "Toaster"};
         string m_Word;
         bool m_Won = false;
         char m_PlayAgain = 'n';
 
         public Hangman()
         {
-            //init
+
+            Random rand = new Random();
+            var ID = rand.Next(0, 2);
+             m_Word = m_Words[ID];
+            m_CorrectLetters = m_Word.ToList();
+
         }
 
         public void Update()
         {
+            ShowHangman();
             char userInput;
+            Console.Write("Please enter your guess: ");
+            userInput = Console.ReadLine()[0];
+            
+            //Checks to see if the user had a correct letter
+            if(m_CorrectLetters.Contains()
+            {
+                Console.WriteLine(userInput.ToString(), ": was a correct answer");
 
+
+            }
             //Check to see if the user has used a value 
             while (m_IncorrectLetters.Contains((userInput = Char.ToLower(Console.ReadKey().KeyChar))))
             {
-                Console.WriteLine(": has already been guessed");
+                Console.WriteLine(userInput.ToString() , ": has already been guessed");
             }
 
             //Clear the screen
